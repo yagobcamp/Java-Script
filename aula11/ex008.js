d895e59d71a591b60f9fceb0f43489eccf6d0db8
yagobcamp <yago.campolongo@gmail.com>
2026-06-11 11:58:10 -0300

    1 a 12

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel} Km/h`);
+    if (vel > 60) { // condição simples!
+console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+    }
+console.log(`Dirija sempre com cinto de segurança!`);
+
+// var vel = 78.2
+// console.log(`A velocidade do seu carro é ${vel}km/h`)
+// if (vel > 60) {  }
+   
