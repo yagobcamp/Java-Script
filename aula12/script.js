b1448578a78877fe2ea4e7edd158eeb39b4b6ef7
yagobcamp <yago.campolongo@gmail.com>
2026-07-25 15:43:05 -0300

    create 12ex

--- aula12/script.js
+++ aula12/script.js
@@ -1,1 +1,16 @@
-alert ('Olá mundo')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = Agora são ${hora} horas.
+    if (hora >= 0 && hora < 12) {
+        // Bom dia
+        img.scr = 'fotomanha.png'
+    } else if (hora >= 12 && hora < 18)
+        //Boa tarde
+        img.scr = 'fototarde.png'
+}   else {
+    // Boa noite
+    img.scr = 'fotonoite.png'
+}
