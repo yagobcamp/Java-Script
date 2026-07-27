51496b7bd3cca64694c33b220b2d3987db5b68bd
yagobcamp <yago.campolongo@gmail.com>
2026-07-27 16:29:25 -0300

    update ex14

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -3,14 +3,15 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = Agora são ${hora} horas.
-    if (hora >= 0 && hora < 12) {
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 5 && hora < 12) {
         // Bom dia
-        img.scr = 'fotomanha.png'
-    } else if (hora >= 12 && hora < 18)
-        //Boa tarde
-        img.scr = 'fototarde.png'
-}   else {
+        img.src = 'modelo/fotomanha.png'
+    }  else if (hora >= 12 && hora < 18) {
+    // Boa tarde
+    img.src = 'modelo/fototarde.png'
+} else {
     // Boa noite
-    img.scr = 'fotonoite.png'
+    img.src = 'modelo/fotonoite.png'
+}
 }
