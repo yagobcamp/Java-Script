4248a12eb197fde4160bf6e8b359bdb16f41b125
yagobcamp <yago.campolongo@gmail.com>
2026-07-27 13:26:24 -0300

    update ex14

--- aula12/script.js
+++ aula12/script.js
@@ -1,16 +1,41 @@
-function carregar() {
+/*function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = Agora são ${hora} horas.
+    //var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // Bom dia
-        img.scr = 'fotomanha.png'
-    } else if (hora >= 12 && hora < 18)
+        img.scr = 'fotomanha'
+    } else if (hora >= 12 && hora <= 18)
         //Boa tarde
         img.scr = 'fototarde.png'
 }   else {
     // Boa noite
     img.scr = 'fotonoite.png'
+}   */
+function carregar() { 
+    var msg = window.document.getElementById('msg') 
+    var img = window.document.getElementById('imagem') 
+    var data = new Date() 
+    var hora = data.getHours() 
+    
+    msg.innerHTML = `Agora são ${hora} horas.` 
+    
+    if (hora >= 0 && hora < 12) { 
+        // Bom dia 
+        img.src = 'modelo/fotomanha.png' // Adicionado "modelo/"
+
+    } else if (hora >= 12 && hora <= 18) { 
+        // Boa tarde 
+        img.src = 'modelo/fototarde.png' // Adicionado "modelo/"
+
+    } else { 
+        // Boa noite 
+        img.src = 'modelo/fotonoite.png' // Adicionado "modelo/"
+        
+    } 
 }
+
+
