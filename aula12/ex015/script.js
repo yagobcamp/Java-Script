6685a15ddd68f9a152f35b7fba5f9879afdf42d3
yagobcamp <yago.campolongo@gmail.com>
2026-07-29 16:16:24 -0300

    create ex015

--- aula12/ex015/script.js
+++ aula12/ex015/script.js
@@ -1,1 +1,30 @@
-alert ('Ola')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById(`txtano`)
+    var res = document.querySelector(`div#res`) 
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert (`[ERRO] Verifique os dados e tente novamente!`)
+    } else {
+        var fses = document.getElementByName('radsex')[0]
+        var idade = ano - Number(fano.value)
+        var genero = ``
+        var img = document.createElement('img')
+        img.setAttribute(`id,` `foto`)
+        if (fsex[0].cheked) {
+            genero = `Homem`
+            if (idade >= 0 && idade < 10) {
+                //Criança
+            } else if (idade < 21) {
+                //Jovem
+            } else if (idade < 50) {
+                //Adulto
+            } else {
+                //Idoso
+            }
+        } else if (fsex[1].cheked) {
+            genero = `Mulher`
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+}
