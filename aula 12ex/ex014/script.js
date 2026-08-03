9f775f4685705588e9131917fdddde3390b346cd
yagobcamp <yago.campolongo@gmail.com>
2026-08-03 16:56:18 -0300

    update js

--- aula 12ex/ex014/script.js
+++ aula 12ex/ex014/script.js
@@ -7,11 +7,14 @@ function carregar() {
     if (hora >= 5 && hora < 12) {
         // Bom dia
         img.src = 'modelo/fotomanha.png'
+        document.body.style.background = '#f1e4c9'
     }  else if (hora >= 12 && hora < 18) {
     // Boa tarde
     img.src = 'modelo/fototarde.png'
+    document.body.style.background = '#b38012'
 } else {
     // Boa noite
     img.src = 'modelo/fotonoite.png'
+    document.body.style.background = '#111b76'
 }
 }
