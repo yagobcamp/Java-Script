444420563e385788fb3ef89551c7305181198c73
yagobcamp <yago.campolongo@gmail.com>
2026-08-04 16:54:12 -0300

    create aula 013

---
+++ aula 13/ambiente.js
@@ -0,0 +1,20 @@
+/*var c = 1
+while ( c < 1000) {
+    console.log('Tudo bem')
+    c++
+}*/
+
+/*var c = 1
+while ( c <= 1000) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+
+    var c =  1
+do { 
+    console.log(`Passo ${c}`)
+    c++
+ }   while (c <= 3)
+
+
