f980a3b0fde68d1103f958f145f1e294dc76edca
yagobcamp <yago.campolongo@gmail.com>
2026-08-09 00:12:57 -0300

    update script.js

--- aula14 ex/ex016/script.js
+++ aula14 ex/ex016/script.js
@@ -7,13 +7,25 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = 1; c <= f; c += p) {
-            res.innerHTML += `${c}`
+            //Passo sempre acima de 1
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
         }
+        if (i < f) {
+            //Contagem crescente
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c}  \u{1F449}`
+        }
+    } else {
+            //Contagem regressiva
+        for(let c = i; c >= f; c -= p) {
+            res.innerHTML += ` ${c}  \u{1F449}`
+        }
+    }
+    res.innerHTML += `\u{1F3C1}` 
     }
 }
