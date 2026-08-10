3820ef891b32a672eeaf79d1a30342ff396253d6
yagobcamp <yago.campolongo@gmail.com>
2026-08-10 15:37:03 -0300

    Update aula15

--- aula15/aula15.js
+++ aula15/aula15.js
@@ -1,21 +1,33 @@
-let valores = [8, 1, 7, 4, 2, 9]
-valores.sort()
+/*let valores = [8, 1, 7, 4, 2, 9]
+valores.sort() 
 //console.log(valores)
-/*
+
 console.log(valores[0])
 console.log(valores[1])
 console.log(valores[2])
 console.log(valores[3])
 console.log(valores[4])
 console.log(valores[5])
-*/
 
-/*
+
+
 for (let pos=0; pos < valores.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}*/
+}
 
 for(let pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}
-    
+}*/
+
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sor()
+        console.log(num)
+        console.log(`O valor tem ${num.length} posições`)
+        console.log(`O primeiro valor é ${num[0]} posições`)
+let pos = num.index0f(8)
+    if (pos == -1) {
+        console.log ('O valor não foi encontrado')
+    } else {
+        console.log(`O valor tem ${num.length} posições`)
+    }
