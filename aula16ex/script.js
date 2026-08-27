1aeb7a6af3bfc16afb0161d4131db92185dc0500
yagobcamp <yago.campolongo@gmail.com>
2026-08-27 10:09:05 -0300

    alter past

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -3,6 +3,7 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+
 function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100) {
         return true
@@ -12,20 +13,17 @@ function isNumero(n) {
 }
 
 function inLista(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
-        return true
+    if(l.indexOf(Number(n)) != -1) {
+        return true 
     } else {
         return false
     }
 }
 
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
-        lista.appendChild(item)
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo Ok')
     } else {
-        window.alert('Valor inválido ou já encontrado na lista.')
+        window.alert('Valor inválido ou já encontrado na lista')
     }
 }
